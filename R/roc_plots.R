81efec791c6eaf15f720f4270c540955dd90fb3d
smeyer-umich <seameyer@umich.edu>
2021-02-14 09:31:33 -0500

    added ROC plot for single model

---
+++ R/roc_plots.R
@@ -0,0 +1,49 @@
+#' Generate an ROC curve plot with error bars showing 95 percent
+#' confidence intervals
+#'
+#' This code builds off of code written by Vincent Guillemot found here:
+#' \url{https://rpubs.com/vguillem/465086}.
+#'
+#' @param df The df as a data.frame.
+#' @param outcome A character string containing the name of the column
+#'   containing the outcomes (expressed as 0/1s).
+#' @param prediction A character string containing the name of the column
+#'   containing the predictions.
+#' @param ci Show confidence interval ribbon.
+#'   Defaults to FALSE.
+#' @param plot_title A character string containing the title for the resulting
+#'   plot.
+#' @return A ggplot containing the calibration plot
+#' @examples
+#' data(single_model_dataset)
+#' roc_plot(single_model_dataset, outcome = 'outcomes', prediction = 'predictions', ci = TRUE)
+#' @export
+#'
+roc_plot <- function(df, outcome, prediction, ci = FALSE, plot_title = '') {
+  obj <- pROC::roc_(df, response = outcome, predictor = prediction, ci = ci, plot=FALSE)
+  ciobj <- pROC::ci.se(obj, specificities = seq(0, 1, l = 25))
+  dat.ci <- data.frame(x = as.numeric(rownames(ciobj)),
+                       lower = ciobj[, 1],
+                       upper = ciobj[, 3])
+
+  g1 = pROC::ggroc(obj) +
+    ggplot2::theme_minimal() +
+    ggplot2::geom_abline(
+      slope = 1,
+      intercept = 1,
+      linetype = "dashed",
+      alpha = 0.7,
+      color = "grey"
+    ) +
+    ggplot2::coord_equal()
+
+  if(ci){
+    g2 = g1 + ggplot2::geom_ribbon(
+      data = dat.ci,
+      ggplot2::aes(x = x, ymin = lower, ymax = upper),
+      fill = "steelblue",
+      alpha = 0.2
+    ) + ggplot2::ggtitle(capture.output(obj$ci))
+  } else g2 = g1 + ggplot2::ggtitle(plot_title)
+  g2
+}
